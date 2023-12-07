0bb0cc87ec6ba00d2ea9d494dd2f54faf4a8a7fd
fizzwic <sakkolly2000@gmail.com>
2023-12-07 15:31:49 -0800

    first commit

---
+++ contracts/TeslaPowerV2.sol
@@ -0,0 +1,23 @@
+//contracts/Tesla.sol
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract TeslaPower {
+    uint256 private horsePower;
+
+    event HorsePowerChanged(uint256 newValue);
+
+    function setHorsePower(uint256 newHorsePower) public {
+        horsePower = newHorsePower;
+        emit HorsePowerChanged(newHorsePower);
+    }
+
+     function incrementHorsePower() public {
+        horsePower++;
+    
+    function retrieveHorsePower() public view return (uint256) {
+        return horsePower;
+    }
+
+   
+}
